3e4fb00e48aa8ef2fd71edfee5e7904817ffbc6a
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-10-18 19:28:35 +0530

    Time: 1 ms (98.63%), Space: 45.2 MB (43.6%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        int last=0;
-        int jump=0;
-        int reach=0;
-        for(int i=0;i<nums.length-1;i++){
-            reach=Math.max(reach,i+nums[i]);
-            if(i==last){
+        int last = 0;
+        int jump = 0;
+        int reach = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            reach = Math.max(reach, i + nums[i]);
+            if (i == last) {
                 jump++;
-                last=reach;
+                last = reach;
             }
         }
         return jump;
         // for(int i=1;i<nums.length;i++){
-        //     if(i>last)
-        //     {
-        //         jump++;
-        //     }
-        //     last=Math.max(last,1+nums[i]);
+        // if(i>last)
+        // {
+        // jump++;
+        // }
+        // last=Math.max(last,1+nums[i]);
         // }
         // return jump;
 
