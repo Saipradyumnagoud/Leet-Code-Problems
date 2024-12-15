e32a87df807ef86e3beeddfba0521cc28b352b2b
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-12-15 17:43:25 +0530

    Time: 2 ms (77.19%), Space: 61.4 MB (74.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int price=prices[0];
-        int profit=0;
-        for(int i=0;i<prices.length;i++){
-            if(price>prices[i]){
-                price=prices[i];
+        int price = prices[0];
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (price > prices[i]) {
+                price = prices[i];
             }
-            profit=Math.max(profit,prices[i]-price);
+            profit = Math.max(profit, prices[i] - price);
         }
         return profit;
     }
