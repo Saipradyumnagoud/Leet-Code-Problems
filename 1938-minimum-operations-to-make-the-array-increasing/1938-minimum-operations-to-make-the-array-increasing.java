0a104614582b4811add6916ea73e57f158b25b6d
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-01-24 19:22:35 +0530

    Time: 4 ms (69.24%), Space: 45.1 MB (49.42%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int count=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<=nums[i-1]){
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
                 int diff = nums[i - 1] - nums[i] + 1;
                 nums[i] += diff;
                 count += diff;
