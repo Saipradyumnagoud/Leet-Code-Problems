55bb1bf5133eaca022d1f30335ce9b508cc358e2
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-01-26 09:17:40 +0530

    Time: 8 ms (14.33%), Space: 45.6 MB (89.13%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
-        List<Integer> result=new ArrayList<>();
-        for(int num:nums){
-            if(frequencyMap.get(num)==1){
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums) {
+            if (frequencyMap.get(num) == 1) {
                 result.add(num);
             }
         }
