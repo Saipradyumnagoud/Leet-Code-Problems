845a070efa7523a453a516166c39373121d180b5
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-04-13 19:50:59 +0530

    Time: 7 ms (69.12%), Space: 44.6 MB (87.56%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> map=new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], map.getOrDefault(words[i], 0) + 1);
+        }
+        List<String> ans=new ArrayList<>(map.keySet());
+        // for(Map.Entry<String,Integer> entry: map.entrySet()){
+        //     if(entry.getValue()==k){
+        //         ans.add(entry.getKey());
+        //     }
+        // }
+
+        ans.sort((a,b)->{
+            int feqcompare=map.get(b)-map.get(a);
+            if(feqcompare==0){
+                return a.compareTo(b);
+            }
+            return feqcompare;
+        });
+
+        return ans.subList(0,k);
+    }
+}
