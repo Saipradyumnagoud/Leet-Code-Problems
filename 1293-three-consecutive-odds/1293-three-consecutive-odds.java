9b7f29e3b2785a91acff9912991f4025fe745284
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-05-11 19:37:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (70.92%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=3;
+        for(int num : arr){
+            if(num%2!=0){
+                count--;
+                if(count==0){
+                    return true;
+                }
+            }
+            if(num%2==0){
+                count=3;
+            }
+        }
+        return false;
+    }
+}
