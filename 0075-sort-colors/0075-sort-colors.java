14a89cb40c654c5aa103a1439de0d7e680ce9694
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-05-17 20:54:27 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Arrays.sort(nums);
-        // System.out.print(nums);
-        int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            int small=i;
-            for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[small]){
-                    small=j;
-                }
-                
-            }
-            int temp = nums[i];
-                nums[i] = nums[small];
-                nums[small] = temp;
-        }
+        Arrays.sort(nums);
+        
     }
 }
