3b03c7b07b5ed6d42b618274d82fb74caccae528
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-07-04 12:40:56 +0530

    Time: 0 ms (100%), Space: 44 MB (97.32%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,31 +13,48 @@
  *     }
  * }
  */
+
+ // if(root==null){
+        //     return 0;
+        // }
+        // Queue<TreeNode> queue=new LinkedList<>();
+        // queue.offer(root);
+        // List<Integer> nums=new ArrayList<>();
+        // while(!queue.isEmpty()){
+        //     int levelSize=queue.size();
+        //     for(int i=0;i<levelSize;i++){
+        //         TreeNode curr=queue.poll();
+        //         nums.add(curr.val);
+        //         if(curr.left!=null){
+        //             queue.offer(curr.left);
+        //         }
+        //         if(curr.right!=null){
+        //             queue.offer(curr.right);
+        //         }
+        //     }
+        // }
+        // Collections.sort(nums);
+        // if(k>nums.size()){
+        //     return -1;
+        // }
+        // return nums.get(k-1);
 class Solution {
+    int count=0;
+    int result=-1;
     public int kthSmallest(TreeNode root, int k) {
+        inorder(root,k);
+        return result;
+    }
+    public void inorder(TreeNode root,int k){
         if(root==null){
-            return 0;
+            return ;
         }
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.offer(root);
-        List<Integer> nums=new ArrayList<>();
-        while(!queue.isEmpty()){
-            int levelSize=queue.size();
-            for(int i=0;i<levelSize;i++){
-                TreeNode curr=queue.poll();
-                nums.add(curr.val);
-                if(curr.left!=null){
-                    queue.offer(curr.left);
-                }
-                if(curr.right!=null){
-                    queue.offer(curr.right);
-                }
-            }
+        inorder(root.left,k);
+        count++;
+        if(count==k){
+            result=root.val;
+            return;
         }
-        Collections.sort(nums);
-        if(k>nums.size()){
-            return -1;
-        }
-        return nums.get(k-1);
+        inorder(root.right,k);
     }
 }
