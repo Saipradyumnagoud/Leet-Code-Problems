e4a01b34989c9f6235beab98b2aeb88a58b08cd6
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2025-12-18 12:22:00 +0530

    Time: 1 ms (25.43%), Space: 53.1 MB (10.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,25 +1,20 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // int left=0;
-        // int right=nums.length-1;
-        // while(left<right){
-        //     int mid=left+(right-left)/2;
-        //     if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
-        //         return nums[mid];
+        
+        // Map<Integer, Integer> fre = new HashMap<>();
+        // for (int num : nums) {
+        //     fre.put(num, fre.getOrDefault(num, 0) + 1);
+        // }
+        // for (Map.Entry<Integer, Integer> entry : fre.entrySet()) {
+        //     if (entry.getValue() == 1) {
+        //         return entry.getKey();
         //     }
-        //     if(nums[mid]==nums[mid+1]){
-        //         mid=mid-1;
-        //     }
-        //}
-        Map<Integer, Integer> fre = new HashMap<>();
-        for (int num : nums) {
-            fre.put(num, fre.getOrDefault(num, 0) + 1);
+        // }
+        // return 0;
+        int ans=0;
+        for(int num : nums){
+            ans^=num;
         }
-        for (Map.Entry<Integer, Integer> entry : fre.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-        return 0;
+        return ans;
     }
 }
